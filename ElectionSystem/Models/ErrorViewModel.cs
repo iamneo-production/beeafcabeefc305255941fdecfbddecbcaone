cf20015c9b14f61dde0a245cfb33e48388fae1d1
Iamneo <techadmin@iamneo.ai>
2023-09-20 05:12:45 +0000

    Backup Commit

---
+++ ElectionSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ElectionSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
